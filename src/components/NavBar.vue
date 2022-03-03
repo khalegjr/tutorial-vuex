abc9dcb5bb2134de7a0717ce1b6789714f37a04a
Júnior Moreira <junior@adunicamp.org.br>
2022-03-03 17:05:10 -0300

    Cria base dos componentes

---
+++ src/components/NavBar.vue
@@ -0,0 +1,7 @@
+<template>Hellow word</template>
+
+<script>
+  export default {};
+</script>
+
+<style></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <ListaUsuario />
+</template>
+
+<script>
+  import ListaUsuario from "@/components/ListaUsuario.vue";
+
+  export default {
+    name: "Home",
+    components: {
+      ListaUsuario,
+    },
+  };
+</script>
+
+<style></style>
