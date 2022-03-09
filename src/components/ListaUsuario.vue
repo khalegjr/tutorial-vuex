d73dc2b690a82d09ff45e0ad088d420e258fbbfa
Júnior Moreira <junior@adunicamp.org.br>
2022-03-09 12:23:10 -0300

    Implementa acesso ao store

--- src/components/ListaUsuario.vue
+++ src/components/ListaUsuario.vue
@@ -19,6 +19,7 @@
 <script>
   import Usuario from "@/components/Usuario.vue";
   import { computed, ref } from "vue";
+  import { usuarioStore } from "../store/usuario";
 
   export default {
     components: {
@@ -26,37 +27,9 @@
     },
 
     setup() {
+      const store = usuarioStore();
       const selecionados = ref([]);
-      const listaPessoas = [
-        {
-          id: 7,
-          email: "michael.lawson@reqres.in",
-          first_name: "Michael",
-          last_name: "Lawson",
-          avatar: "https://reqres.in/img/faces/7-image.jpg",
-        },
-        {
-          id: 8,
-          email: "lindsay.ferguson@reqres.in",
-          first_name: "Lindsay",
-          last_name: "Ferguson",
-          avatar: "https://reqres.in/img/faces/8-image.jpg",
-        },
-        {
-          id: 9,
-          email: "tobias.funke@reqres.in",
-          first_name: "Tobias",
-          last_name: "Funke",
-          avatar: "https://reqres.in/img/faces/9-image.jpg",
-        },
-        {
-          id: 10,
-          email: "byron.fields@reqres.in",
-          first_name: "Byron",
-          last_name: "Fields",
-          avatar: "https://reqres.in/img/faces/10-image.jpg",
-        },
-      ];
+      const listaPessoas = store.listaPessoas;
 
       const nomeSelecionados = computed(() => {
         return selecionados.value.map((x) => `${x.first_name} ${x.last_name}`);
--- src/views/Favoritos.vue
+++ src/views/Favoritos.vue
@@ -17,44 +17,17 @@
 
 <script>
   import { onMounted, ref } from "vue";
+  import { usuarioStore } from "../store/usuario";
 
   export default {
     components: {},
 
     setup() {
+      const store = usuarioStore();
       const listaFavoritos = ref([]);
 
       onMounted(() => {
-        listaFavoritos.value = [
-          {
-            id: 7,
-            email: "michael.lawson@reqres.in",
-            first_name: "Michael",
-            last_name: "Lawson",
-            avatar: "https://reqres.in/img/faces/7-image.jpg",
-          },
-          {
-            id: 8,
-            email: "lindsay.ferguson@reqres.in",
-            first_name: "Lindsay",
-            last_name: "Ferguson",
-            avatar: "https://reqres.in/img/faces/8-image.jpg",
-          },
-          {
-            id: 9,
-            email: "tobias.funke@reqres.in",
-            first_name: "Tobias",
-            last_name: "Funke",
-            avatar: "https://reqres.in/img/faces/9-image.jpg",
-          },
-          {
-            id: 10,
-            email: "byron.fields@reqres.in",
-            first_name: "Byron",
-            last_name: "Fields",
-            avatar: "https://reqres.in/img/faces/10-image.jpg",
-          },
-        ];
+        listaFavoritos.value = store.listaFavoritos;
       });
 
       function removeFavorito(idUsuario) {
