d571ff3e8914f8f3cffe650d24d4b72e8af0b9f6
Júnior Moreira <junior@adunicamp.org.br>
2022-03-09 11:22:15 -0300

    Refatora store para organização em módulos

---
+++ src/store/usuario/getters.js
@@ -0,0 +1,9 @@
+export default {
+  totalFavoritos(state) {
+    return state.listaFavoritos.length;
+  },
+
+  isFavorito: (state) => (id) => {
+    return state.listaFavoritos.some((x) => x.id === id);
+  },
+};
